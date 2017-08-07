143ecf88cb798a4f332338b21782de4a09532a78
lizdenhup <lizdenhup@gmail.com>
2017-08-07 10:36:07 -0500

    not working yet

--- examples/mergesort.js
+++ examples/mergesort.js
@@ -1,14 +1,12 @@
 var unsortedArray = [234, 243, 4, 77, 8, 9, 9, 10, 1232, 33, 45, 56, 22, 1]
 
 function merge(leftArr, rightArr) {
+    var sortedArr = [];
     if (leftArr && rightArr) {
-        sortedArr = [];
-        for (var i = 0; i < leftArr.length; i++) {
-            if (leftArr[i] <= rightArr[i]) {
-                sortedArr.push(leftArr[i])
-            } else {
-                sortedArr.push(rightArr[i])
-            }
+        if (leftArr[0] <= rightArr[0]) {
+            sortedArr.push(leftArr[0])
+        } else {
+            sortedArr.push(rightArr[0])
         }
     } else if (rightArr && !leftArr) {
         sortedArr.push(...rightArr);
@@ -22,28 +20,12 @@ function mergesort(arr) {
     if (arr.length < 2) {
         console.log('The list was so small that it is already sorted!')
         return arr;
-    } else {
-        if (arr.length % 2 === 0) {
-            var midpoint = arr.length/2
-            var leftArr = arr.slice(arr[0], arr[midpoint])
-            var rightArr = arr.slice(arr[midpoint])
-            mergesort(leftArr)
-            mergesort(rightArr)
-            var output = merge(leftArr, rightArr)
-    } else {
-            var midpoint = (arr.length + 1)/2
-            var leftArr = arr.slice(arr[0], arr[midpoint])
-            var rightArr = arr.slice(arr[midpoint])
-            mergesort(leftArr)
-            mergesort(rightArr)
-            var output = merge(leftArr, rightArr)
-        }
-        console.log(output)
-        return output 
-    }
+        var midpoint = parseInt(arr.length/2)
+        var leftArr = arr.slice(0, midpoint)
+        var rightArr = arr.slice(midpoint)
+        return merge(mergesort(leftArr), mergesort(rightArr)
+        )
+    } 
 }
 
-function split(arr) {
-
-}
-
+console.log(mergesort(unsortedArray))
