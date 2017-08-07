f5153c6b08daac6a067d7803bea7013924cc298c
lizdenhup <lizdenhup@gmail.com>
2017-08-07 10:09:36 -0500

    completed mergesort implementation, need to test

--- examples/mergesort.js
+++ examples/mergesort.js
@@ -1,36 +1,49 @@
+var unsortedArray = [234, 243, 4, 77, 8, 9, 9, 10, 1232, 33, 45, 56, 22, 1]
+
 function merge(leftArr, rightArr) {
     if (leftArr && rightArr) {
         sortedArr = [];
         for (var i = 0; i < leftArr.length; i++) {
-            for (var j = 0; j < rightArr.length; j++) {
-
+            if (leftArr[i] <= rightArr[i]) {
+                sortedArr.push(leftArr[i])
+            } else {
+                sortedArr.push(rightArr[i])
             }
         }
+    } else if (rightArr && !leftArr) {
+        sortedArr.push(...rightArr);
+    } else {
+        sortedArr.push(...leftArr);
     }
+    return sortedArr; 
 }
 
 function mergesort(arr) {
     if (arr.length < 2) {
-        return arr;
         console.log('The list was so small that it is already sorted!')
+        return arr;
     } else {
-        //split the list repeatedly until it is single elements
-        //merge the elements
-        //continue doing this until you get the whole list back, but sorted 
+        if (arr.length % 2 === 0) {
+            var midpoint = arr.length/2
+            var leftArr = arr.slice(arr[0], arr[midpoint])
+            var rightArr = arr.slice(arr[midpoint])
+            mergesort(leftArr)
+            mergesort(rightArr)
+            var output = merge(leftArr, rightArr)
+    } else {
+            var midpoint = (arr.length + 1)/2
+            var leftArr = arr.slice(arr[0], arr[midpoint])
+            var rightArr = arr.slice(arr[midpoint])
+            mergesort(leftArr)
+            mergesort(rightArr)
+            var output = merge(leftArr, rightArr)
+        }
+        console.log(output)
+        return output 
     }
 }
 
-var unsortedArray = [234, 243, 4, 77, 8, 9, 9, 10, 1232, 33, 45, 56, 22, 1]
-
 function split(arr) {
-    if (arr.length % 2 === 0) {
-        var midpoint = arr.length/2
-        leftArr = arr.slice(arr[0], arr[midpoint])
-        rightArr = arr.slice(arr[midpoint])
-    } else {
-        var midpoint = (arr.length + 1)/2
-        leftArr = arr.slice(arr[0], arr[midpoint])
-        rightArr = arr.slice(arr[midpoint])
-    }
+
 }
 
