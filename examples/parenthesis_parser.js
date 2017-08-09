2150e671f3a2c9153df467a5d21115bfa611d13a
lizdenhup <lizdenhup@gmail.com>
2017-08-09 15:36:04 -0500

    working solution with stack

--- examples/parenthesis_parser.js
+++ examples/parenthesis_parser.js
@@ -1,37 +1,28 @@
-// given a string of parenthesis return a boolean 'true' if the 
-// string contains valid sets of parenthesis or 'false' if it does
-// not. You should only traverse the string array once to solve this. 
-// input examples with their corresponding return values:
-// '{[]]}' => false
-// '}[()' => false
-// '{[()]}' => true
-// '()' => true 
-
-const parentheses = {
-    '{': '}',
-    '[': ']',
-    '(': ')'
-}
+//study stacks further 
+var map = {
+    "(": ")",
+    "[": "]",
+    "{": "}"
+} 
+//create a map which relates left and right parentheses 
 
 var isValid = function(s) {
-    if (!s.length || s.length % 2 !== 0) {
-        console.log('Either there is no string or the string length is not a multiple of two so it cannot be valid')
-        return false; 
-    } else {
-        let charArr = [...s] // turns string into array of char strings 
-        for (var i = 0; j = charArr.length; i < j) {
-            if (parentheses.hasOwnProperty(charArr[i])) {
-                //check to see if char at index i is a key, if so keep going 
-                continue 
-            } else {
-                // check if the preceding char is a key for the value
-                if (parentheses.hasOwnProperty(charArr[i - 1]) && parentheses[charArr[i -1]] === charArr[i]) {
-                    charArr = charArr.splice(0, i - 1).concat(charArr.splice(i + 1)) 
-                    return true 
-                }
+    var stack = [];
+    //a stack is a data structure that supports 3 operations: adding to the stack, removing from the stack, displaying the stack 
+    //contents. a basic stack is implemented using a 'last in, first out' approach.
+    for (var i = 0; i < s.length; i++) {
+        var item = s[i];
+        //item is a variable whose value is the element at position i of the string 
+        if (map[item]) {
+            //if item is a value, add that value ie '}' to the stack
+            stack.push(map[item]);
+        } else {
+            if (item !== stack.pop()) {
+                //if item is not equal to the first element of the stack return false 
+                return false;
             }
         }
     }
+    //return the stack when its elements have been exhausted 
+    return stack.length === 0;
 };
-
-//currently passing 2/73 test cases on leetcode before timing out
